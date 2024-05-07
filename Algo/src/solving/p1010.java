217edf65a18c1bcd650494376200f28876a7bfa4
codeVac513 <xssdsw@naver.com>
2024-05-07 20:56:19 +0900

    백준 2748 해결

---
+++ Algo/src/solving/p1010.java
@@ -0,0 +1,24 @@
+package solving;
+
+import java.io.*;
+
+public class p1010 {
+    //1. 입력 생각하기
+    //
+    //2. 구현 방법
+    //
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+
+        for(int testCase = 0 ; testCase < t; testCase++) {
+            String[] nums = br.readLine().split(" ");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
